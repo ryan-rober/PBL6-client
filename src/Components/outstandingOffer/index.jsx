05cc0420d70a2e051206ee10fe8337fc0b7de6b5
Ryan Zh <92838570+hwangzh@users.noreply.github.com>
2023-10-24 06:58:12 +0700

    Create outstanding offer component

---
+++ src/Components/outstandingOffer/index.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { item } from "./item";
+
+const Wrapper = styled.header`
+  .wid-4 {
+    width: 20rem !important;
+  }
+  .scroll {
+    overflow: auto !important;
+  }
+`;
+
+const OutstandingOffer = () => {
+  return (
+    <Wrapper className='w-3/5 m-auto mt-10'>
+      <div>
+        <h1 className='text-lg'>Ưu đãi nổi bật </h1>
+        <div className="flex py-[5px] justify-center scroll">
+          {item?.map((item, index) => (
+            <div className="m-2 shadow-md rounded-lg " style={{ marginLeft: item.style }} key={index}>
+              <div className=" rounded-lg shadow-sm wid-4">
+                <a
+                  href=''
+                  className=" h-36 overflow-hidden rounded-t-lg relative block"
+                >
+                  <img
+                    src={item.url}
+                    alt=""
+                    className="w-full h-full absolute inset-0 object-cover"
+                  />
+                </a>
+                <div className="p-3 bg-white rounded-b-lg text-black">
+                  <p className="block text-base font-semibold text-current line-clamp-1 mb-1">
+                    {item.title}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default OutstandingOffer;
---
+++ src/Components/outstandingOffer/item.jsx
@@ -0,0 +1,26 @@
+export const item = [
+	{
+		href: "https://vexere.com/flash-sale-ve-xe?utm_source=banner&utm_medium=cms&utm_campaign=FS181022_banner_cms_oct2022",
+		url: "https://storage.googleapis.com/vex-config/cms-tool/post/images/163/img_card.png",
+		title : "Ưu đãi các tuyến đường HOT lên đến 50%",
+		style: '25rem'
+	},
+    {
+		href:"https://vexere.com/vi-VN/bai-viet/uu-dai-khach-hang-moi",
+		url: "https://storage.googleapis.com/vex-config/cms-tool/post/images/161/img_card.png",
+		title: "Dành cho KH mới - Giảm 25% khi đặt vé",
+		style: '0'
+	},
+    {
+		href:"https://vexere.com/khuyen-mai/?utm_source=banner&utm_medium=cms&utm_campaign=khuyenmai_2022",
+		url: "https://storage.googleapis.com/vex-config/cms-tool/post/images/137/img_card.png",
+		title: "Tổng hợp chương trình khuyến mãi trong tháng",
+		style: '0'
+	},
+    {
+		href:"https://vexere.com/vi-VN/bai-viet/uu-dai-bat-ngo",
+		url: "https://storage.googleapis.com/vex-config/cms-tool/post/images/152/img_card.png",
+		title: "Ưu đãi bất ngờ khi đặt vé",
+		style: '0'
+	}
+]
