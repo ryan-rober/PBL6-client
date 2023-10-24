b42f49e97c0dff833f3fa0a1ca8fadef0ee54fd6
Ryan Zh <92838570+hwangzh@users.noreply.github.com>
2023-10-24 08:21:00 +0700

    Create i18n translate

---
+++ src/I18n/translate/jp/index.js
@@ -0,0 +1,11 @@
+import common from './common.json'
+import course from './course.json'
+import errorMessage from './error_message.json'
+import menu from './menu.json'
+
+export default {
+  common,
+  course,
+  error_message: errorMessage,
+  menu
+}
