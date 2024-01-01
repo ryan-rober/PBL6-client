e3bcbcce13b797a7f0d71a7033a0a4614ab948f2
Ryan Zh <92838570+ryan-rober@users.noreply.github.com>
2024-01-01 15:53:27 +0700

    Header fix

--- src/Components/header/item.tsx
+++ src/Components/header/item.tsx
@@ -5,28 +5,28 @@ export const navList = [
 		path: "/",
 		isActive: true
 	},
-    // {
-	// 	id:'2',
-	// 	name: "Tin tức",
-	// 	path: "/tintuc",
-	// 	isActive: false
-	// },
-    // {
-	// 	id:'3',
-	// 	name: "Giới thiệu",
-	// 	path: "/gioithieu",
-	// 	isActive: false
-	// },
-    // {
-	// 	id:'4',
-	// 	name: "Liên hệ",
-	// 	path: "/lienhe",
-	// 	isActive: false
-	// },
-    // {
-	// 	id:'5',
-	// 	name: "Đánh giá",
-	// 	path: "/danhgia",
-	// 	isActive: false
-	// }
+    {
+		id:'2',
+		name: "Tin tức",
+		path: "/tintuc",
+		isActive: false
+	},
+    {
+		id:'3',
+		name: "Giới thiệu",
+		path: "/gioithieu",
+		isActive: false
+	},
+    {
+		id:'4',
+		name: "Liên hệ",
+		path: "/lienhe",
+		isActive: false
+	},
+    {
+		id:'5',
+		name: "Đánh giá",
+		path: "/danhgia",
+		isActive: false
+	}
 ]
